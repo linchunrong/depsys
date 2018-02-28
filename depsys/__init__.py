a02822ba3af37e623342d941738647e80e0e16ff
Ryan Lin <ryanlin85.cn@gmail.com>
2018-02-28 18:30:03 +0800

    update db config

--- depsys/__init__.py
+++ depsys/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('depsys.setting')
+app.config.from_envvar('FLASKR_SETTINGS')
+
+db = SQLAlchemy(app)

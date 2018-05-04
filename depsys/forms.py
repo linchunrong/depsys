d5a6445d526cb4ac7dfb680987ecd007ce85d102
Ryan Lin <ryanlin85.cn@gmail.com>
2018-05-04 17:22:49 +0800

    add config form for config_project

--- depsys/forms.py
+++ depsys/forms.py
@@ -2,11 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from flask_wtf import FlaskForm
-from wtforms import BooleanField,StringField,SubmitField,PasswordField,validators
+from wtforms import BooleanField, StringField, SubmitField, PasswordField, validators, SelectField, TextAreaField
 
 class LoginForm(FlaskForm):
     """Login Form"""
-    username = StringField('Username', validators=[validators.DataRequired()])
-    password = PasswordField('Password', validators=[validators.DataRequired()])
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Login')
+    username = StringField('Username', validators=[validators.InputRequired()], render_kw={"placeholder": "用户名"})
+    password = PasswordField('Password', validators=[validators.InputRequired()], render_kw={"placeholder": "密码"})
+    #remember_me = BooleanField('Remember me')
+    #submit = SubmitField('Login')
+
+class ConfigForm(FlaskForm):
+    """Config Form"""
+    project_name = StringField('Project_name', validators=[validators.InputRequired()], render_kw={"placeholder": "请输入新工程名(必填)"})
+    servers = StringField('IPs', validators=[validators.InputRequired()], render_kw={"placeholder": "多个IP请以 \",\" 号分隔！(必填)", "style": "width: 60%"})
+    source_address = StringField('Repository_address', validators=[validators.InputRequired()], render_kw={"style": "width: 60%"})
+    deploy_script = StringField('Deploy_script', validators=[validators.InputRequired()], render_kw={"style": "width: 60%"})
+    start_script = StringField('Start_script', validators=[validators.InputRequired()], render_kw={"style": "width: 60%"})
+    stop_script = StringField('Stop_script', validators=[validators.InputRequired()], render_kw={"style": "width: 60%"})
+    post_script_type = SelectField('Post_script_type', choices=[('shell','shell script'),('python','python script')])
+    post_script = TextAreaField('Post_script', render_kw={"placeholder": "请在此框输入完整脚本！", "style": "width: 80%; height: 50px" })
+    #submit = SubmitField('Save')

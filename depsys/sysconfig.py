2df61fae1a980f239fc9e74cba81eca151bc2ded
Ryan Lin <ryanlin85.cn@gmail.com>
2018-05-21 17:46:48 +0800

    update

--- depsys/sysconfig.py
+++ depsys/sysconfig.py
@@ -2,22 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from depsys import db
-from depsys.models import Project
+from depsys.models import Project, System
 
-def system_config():
-    pass
+class System_config:
+    """System config, only have update method"""
+    def update(self,ansible_path):
+        # Should be only one record in DB
+        item = System.query.first()
+        item.ansible_path = ansible_path
+        db.session.commit()
 
 class Project_config:
     """ Action for project"""
     def update(self,project_name_old, project_name, servers, source_address, post_script_type, post_script):
         """Update project config"""
         if project_name_old:
-            update_project = Project.query.filter_by(project_name=project_name_old).first()
-            update_project.project_name = project_name
-            update_project.servers = servers
-            update_project.source_address = source_address
-            update_project.post_script_type = post_script_type
-            update_project.post_script = post_script if post_script else None
+            item = Project.query.filter_by(project_name=project_name_old).first()
+            item.project_name = project_name
+            item.servers = servers
+            item.source_address = source_address
+            item.post_script_type = post_script_type
+            item.post_script = post_script if post_script else None
             db.session.commit()
 
     def add(self, project_name, servers, source_address, post_script_type, post_script):

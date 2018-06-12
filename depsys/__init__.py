dabc6bcadc3cb0ea69826c3e08a848a6244efce2
Ryan Lin <ryanlin85.cn@gmail.com>
2018-06-12 18:44:23 +0800

    using flask-socketio

--- depsys/__init__.py
+++ depsys/__init__.py
@@ -4,14 +4,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_socketio import SocketIO
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
 
 app = Flask(__name__)
 app.config.from_object('depsys.setting')
-#app.config.from_envvar('FLASKR_SETTINGS')
+# app.config.from_envvar('FLASKR_SETTINGS')
+socketio = SocketIO(app, async_mode=async_mode)
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
-login_manager.session_protection='strong'
+login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
--- runserver.py
+++ runserver.py
@@ -1,7 +1,8 @@
 #!/use/bin/env python
 # -*- coding: utf-8 -*-
 
-from depsys import app
+from depsys import app, socketio
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    socketio.run(app,debug=True)

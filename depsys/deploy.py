5bc011ea3dc06ee594aa9b77c56326fc85e677c8
Ryan Lin <ryanlin85.cn@gmail.com>
2018-06-26 20:57:48 +0800

    update

--- depsys/deploy.py
+++ depsys/deploy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib.request, time, os, random, string
+import urllib.request, time, os, random, string, pathlib
 from threading import Lock
 from depsys import socketio
 from flask_socketio import disconnect, emit
@@ -10,6 +10,7 @@ from depsys.sysconfig import SystemConfig
 # deploy execute process
 thread = None
 thread_lock = Lock()
+temp_path = "tmp"
 
 
 @socketio.on('disconnect_request', namespace='/execute')
@@ -30,17 +31,23 @@ def execute_do():
 
 def execute_thread():
     """Example of how to send server generated events to clients."""
-    count = 0
     while True:
         socketio.sleep(3)
-        count += 1
         socketio.emit('my_response',
-                      {'time_stamp': time.strftime("%Y-%m-%d:%H:%M:%S",time.localtime()), 'data': "Executing..."},
+                      {'time_stamp': time.strftime("%Y-%m-%d:%H:%M:%S",time.localtime()), 'data': str(create_playbook())},
                       namespace='/execute')
+
+
+def my_path():
+    """Get current file path"""
+    root = os.path.dirname(os.path.realpath(__file__))
+    root = pathlib.Path(root)
+    return root
 
 
 def mkdir(path):
     path = path.strip()
+    os.chdir(my_path())
     isExists = os.path.exists(path)
     if isExists:
         # print (path +  "目录已存在！")
@@ -63,20 +70,27 @@ def get_script():
     remote_script = conf.get().deploy_script
     # check if script path is set in remote http url
     if 'http' in remote_script:
-        path = "tmp"
-        mkdir(path)
-        local_script = path + "/deploy_script.sh"
+        os.chdir(my_path())
+        local_script = temp_path + "/deploy_script.sh"
         urllib.request.urlretrieve(remote_script, filename=local_script)
-        return local_script
+        return str(local_script)
     # otherwise, script path is set in local path by default
     else:
         local_script = remote_script
-        return local_script
+        return str(local_script)
 
 
 def create_playbook():
     """Create ansible playbook"""
-    src_file = "yml/execute.yml"
+    os.chdir(my_path())
+    mkdir(temp_path)
+    src_file = my_path()/"yml/execute.yml"
+    #file_name = "playbook_" + random_string(16) + ".yml"
+    file_name = "playbook.yml"
     with open(src_file) as src:
-        playbook = src.read()
-        print (playbook)
+        playbook_template = src.read()
+    os.chdir(temp_path)
+    with open(file_name, 'w+') as dest:
+        dest.write(playbook_template.replace("local_script_path", get_script()))
+        playbook = dest.read()
+    return playbook

e8f0c884dc840c4a2247112b4f7da2ca81b0f97e
Ryan Lin <ryanlin85.cn@gmail.com>
2018-06-27 17:29:51 +0800

    update

--- depsys/deploy.py
+++ depsys/deploy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib.request, time, os, random, string, pathlib
+import urllib.request, time, os, random, string, pathlib, subprocess
 from threading import Lock
 from depsys import socketio
 from flask_socketio import disconnect, emit
@@ -11,6 +11,7 @@ from depsys.sysconfig import SystemConfig
 thread = None
 thread_lock = Lock()
 temp_path = "tmp"
+logs_path = "logs"
 
 
 @socketio.on('disconnect_request', namespace='/execute')
@@ -31,10 +32,14 @@ def execute_do():
 
 def execute_thread():
     """Example of how to send server generated events to clients."""
-    while True:
-        socketio.sleep(3)
-        socketio.emit('my_response',
-                      {'time_stamp': time.strftime("%Y-%m-%d:%H:%M:%S",time.localtime()), 'data': str(create_playbook())},
+    run_ansible()
+    running = True
+    with open("logs.txt") as logs:
+        while running:
+            output = str(logs.read())
+            socketio.sleep(2)
+            socketio.emit('my_response',
+                      {'time_stamp': time.strftime("%Y-%m-%d:%H:%M:%S",time.localtime()), 'data': output},
                       namespace='/execute')
 
 
@@ -47,9 +52,9 @@ def my_path():
 
 def mkdir(path):
     path = path.strip()
-    os.chdir(my_path())
-    isExists = os.path.exists(path)
-    if isExists:
+    os.chdir(str(my_path()))
+    exists = os.path.exists(path)
+    if exists:
         # print (path +  "目录已存在！")
         return False
     else:
@@ -70,7 +75,7 @@ def get_script():
     remote_script = conf.get().deploy_script
     # check if script path is set in remote http url
     if 'http' in remote_script:
-        os.chdir(my_path())
+        os.chdir(str(my_path()))
         local_script = temp_path + "/deploy_script.sh"
         urllib.request.urlretrieve(remote_script, filename=local_script)
         return str(local_script)
@@ -82,15 +87,31 @@ def get_script():
 
 def create_playbook():
     """Create ansible playbook"""
-    os.chdir(my_path())
+    os.chdir(str(my_path()))
     mkdir(temp_path)
     src_file = my_path()/"yml/execute.yml"
-    #file_name = "playbook_" + random_string(16) + ".yml"
-    file_name = "playbook.yml"
+    dest_file = "playbook_" + random_string(16) + ".yml"
+    # read playbook template =
     with open(src_file) as src:
         playbook_template = src.read()
+    # cd to temporary folder and write a temporary playbook
     os.chdir(temp_path)
-    with open(file_name, 'w+') as dest:
-        dest.write(playbook_template.replace("local_script_path", get_script()))
-        playbook = dest.read()
-    return playbook
+    with open(dest_file, 'w+') as dest:
+        content = playbook_template.replace("local_script_path", get_script())
+        dest.write(content)
+    # read out the temporary playbook for executing
+    #with open(dest_file) as new:
+    #    playbook = new.read()
+
+    return dest_file
+
+
+def run_ansible():
+    """execute ansible command for deploy"""
+    os.chdir(str(my_path()))
+    mkdir(logs_path)
+    os.chdir(logs_path)
+    # write logs into temporary log files
+    command = "ping www.baidu.com -n 5"
+    with open("logs.txt", "w+") as file:
+        subprocess.Popen(command, stdout=file)

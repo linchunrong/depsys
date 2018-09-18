c5f70f11d3f8a8aa94209d9a0ae827b4fe80d9bd
Ryan Lin <ryanlin85.cn@gmail.com>
2018-09-18 18:55:59 +0800

    fix format of html context

--- depsys/makemsg.py
+++ depsys/makemsg.py
@@ -24,51 +24,53 @@ class Report:
 
     def make_html(self, records_list):
         """Format content to html table"""
-        html_head = """
-        <meta http-equiv=Content-Type content=text/html; charset=utf-8 />
-        <html lang=en>
-            <h1>最近""" + len(records_list) + """天发布记录</h1>
-        """
+        html_head = """\
+        <meta http-equiv=Content-Type content=text/html; charset=utf-8 />\
+        <html lang=en>\
+            <h1>最近""" + str(len(records_list)) + """天发布记录</h1>\
+            """
+        html_tables = ""
         for records in records_list:
             if records['num'] == 0:
-                html_table = """
-                <p>""" + records['date'] + """</p>
-                <p>----无发布记录。----</p>
+                html_table = """\
+                <p>""" + records['date'] + """</p>\
+                <p>----无发布记录。----</p>\
                 """
             else:
                 html_table = """
-                <p>2018-09-13 Thursday<p>
-                <TABLE BORDER=1>
-                    <TR>
-                        <TH>当日发布工程数</TH>
-                    </TR>
-                    <TR>
-                        <TD>""" + records['num'] + """</TD>
-                    </TR>
-                </TABLE>
-                <TABLE BORDER=1>
-                    <TR>
-                        <TH>发布日期</TH>
-                        <TH>发起人</TH>
-                        <TH>工程</TH>
-                        <TH>发布原因</TH>
-                    </TR>
+                <p>""" + records['date'] + """<p>\
+                <TABLE BORDER=1>\
+                    <TR>\
+                        <TH>当日发布工程数</TH>\
+                    </TR>\
+                    <TR>\
+                        <TD>""" + str(records['num']) + """</TD>\
+                    </TR>\
+                </TABLE>\
+                <TABLE BORDER=1>\
+                    <TR>\
+                        <TH>发布日期</TH>\
+                        <TH>发起人</TH>\
+                        <TH>工程</TH>\
+                        <TH>发布原因</TH>\
+                    </TR>\
                 """
-                for record in records:
+                for record in records['records']:
                     project = Project.query.filter_by(project_id=record.project_id).first()
                     project_name = project.project_name
-                    html_table = html_table + """
-                    <TR>
-                        <TD>""" + record.time_begin + """</TD>
-                        <TD>""" + record.requester + """</TD>
-                        <TD>""" + project_name + """</TD>
-                        <TD>""" + record.deploy_reason + """</TD>
-                    </TR>
+                    html_table = html_table + """\
+                    <TR>\
+                        <TD>""" + str(record.time_begin) + """</TD>\
+                        <TD>""" + record.requester + """</TD>\
+                        <TD>""" + project_name + """</TD>\
+                        <TD>""" + record.deploy_reason + """</TD>\
+                    </TR>\
                     """
-                html_table = html_table + """
-                </TABLE>
+                html_table = html_table + """\
+                </TABLE>\
                 """
-        html = html_head + html_table + """</html>"""
+            html_tables = html_tables + html_table
+        html = html_head + html_tables + """</html>"""
 
         return html
 

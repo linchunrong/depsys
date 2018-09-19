5f0c149dd54fffbd65aaceb794ae70ba0088d4e0
Ryan Lin <ryanlin85.cn@gmail.com>
2018-09-19 17:47:38 +0800

    update email content

--- depsys/sendmsg.py
+++ depsys/sendmsg.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
-import os
-import smtplib
+import time, smtplib, os
 from email.message import EmailMessage
 from depsys.sysconfig import SystemConfig
 
 
-def email(receiver, attachment=None):
+def email(receiver, attachment=None, content='FYI', subtype='plain'):
     """Send email"""
     conf = SystemConfig().get()
     mail_host = conf.smtp_server
-    fromaddr = conf.smtp_user
-    # frompass = conf.smtp_pwd
-    sender = fromaddr
+    # auth_user = conf.smtp_user
+    # auth_pass = conf.smtp_pwd
+    sender = conf.smtp_user
     tolist = receiver
 
     message = EmailMessage()
     message['From'] = sender
     message['To'] = tolist
     subject = 'Deploy info ' + time.strftime("%Y-%m-%d", time.localtime())
-    message['subject'] = subject
-    message.set_content("FYI")
+    message['Subject'] = subject
+    message.set_content(content, subtype=subtype)
 
     if attachment:
         message.add_attachment(open(attachment, 'r').read(), subtype='plane', filename=os.path.basename(attachment))
 
     try:
         s = smtplib.SMTP(mail_host, 25)
-        # s.login(fromaddr, frompass)
+        # s.set_debuglevel(1)
+        # s.ehlo()
+        # s.starttls()
+        # s.login(auth_user, auth_pass)
         s.send_message(message, sender, tolist)
         s.quit()
-        return "Send success!"
-
     except smtplib.SMTPException as e:
         return "Error: " + str(e)
+    else:
+        return "Send success!"
